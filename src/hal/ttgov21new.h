f712406846d4ac2ca45f4be957f97a0f49afcd9c
Luigi Frassato <luigifrassato@outlook.com>
2020-09-07 02:04:06 -0300

    change oled backend

--- src/hal/ttgov21new.h
+++ src/hal/ttgov21new.h
@@ -29,7 +29,7 @@
 //#define HAS_BME 1 // Enable BME sensors in general
 //#define HAS_BME280 GPIO_NUM_21, GPIO_NUM_22 // SDA, SCL
 
-#define HAS_DISPLAY 2
+#define HAS_DISPLAY 1
 #define HAS_LED (25) // green on board LED
 #define BAT_MEASURE_ADC ADC1_GPIO35_CHANNEL // battery probe GPIO pin -> ADC1_CHANNEL_7
 #define BAT_VOLTAGE_DIVIDER 2 // voltage divider 100k/100k on board

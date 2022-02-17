dd805025ce0d73034ca3c0b86e1aac7d7f32ddad
danielei9 <dbs99@hotmail.com>
2022-02-17 21:58:48 +0100

    WORKING

--- src/macsniff.cpp
+++ src/macsniff.cpp
@@ -41,7 +41,16 @@ uint64_t macConvert(uint8_t *paddr) {
   mac = (uint64_t *)paddr;
   return (__builtin_bswap64(*mac) >> 16);
 }
-
+// converts character array
+// to string and returns it
+String convertToString(char *a, int size) {
+  int i;
+  String s = "";
+  for (i = 0; i < size; i++) {
+    s = s + a[i];
+  }
+  return s;
+}
 bool mac_add(uint8_t *paddr, int8_t rssi, uint8_t sniff_type) {
   if (!salt) // ensure we have salt (appears after radio is turned on)
     return false;
@@ -57,106 +66,145 @@ bool mac_add(uint8_t *paddr, int8_t rssi, uint8_t sniff_type) {
 
   // use OUI vendor filter list only on Wifi, not on BLE
   if ((sniff_type == MAC_SNIFF_BLE) || (sniff_type == MAC_SNIFF_BT) ||
-      std::find(vendors_list.begin(), vendors_list.end(), __builtin_bswap32(*oui) >> 8) !=
-          vendors_list.end()) {
+      std::find(vendors_list.begin(), vendors_list.end(),
+                __builtin_bswap32(*oui) >> 8) != vendors_list.end()) {
 #endif
 
     // salt and hash MAC, and if new unique one, store identifier in container
     // and increment counter on display
     // https://en.wikipedia.org/wiki/MAC_Address_Anonymization
 
-    //ESP_LOGI(TAG, "MAC is: %02X%02X%02X%02X%02X%02X", paddr[0],paddr[1],paddr[2],paddr[3],paddr[4],paddr[5]);
-    snprintf(buff, sizeof(buff), "%02X%02X%02X%02X%02X%02X",
-             paddr[0],paddr[1],paddr[2],paddr[3],paddr[4],paddr[5]);
-    //ESP_LOGI(TAG, "Content of buff is: %s", buff);
-                   // convert unsigned 32-bit salted MAC
-     /*                                     // to 8 digit hex string
-    hashedmac = rokkit(&buff[0], 5);      // hash MAC 8 digit -> 5 digit*/
+    /*  ESP_LOGI(TAG, "MAC is: %02X%02X%02X%02X%02X%02X",
+      paddr[0],paddr[1],paddr[2],paddr[3],paddr[4],paddr[5]);*/
+   String cadena = buff;
+    
+    snprintf(buff, sizeof(buff), "%02X:%02X:%02X:%02X:%02X:%02X", paddr[0],
+             paddr[1], paddr[2], paddr[3], paddr[4], paddr[5]);
+    ESP_LOGI(TAG, "Content of buff is: %s", buff);
+    //  convert unsigned 32-bit salted MAC
+    // to 8 digit hex string
+    // hashedmac = rokkit(&buff[0], 5);      // hash MAC 8 digit -> 5 digit
     char out[METIS_OUTPUT_HASH_LENGTH];
-    uint8_t in[METIS_OUTPUT_HASH_LENGTH];
-    //for(int n = 0; n < METIS_OUTPUT_HASH_LENGTH; n++) in[n] = 0;
-   // for(int n = 0; n < METIS_OUTPUT_HASH_LENGTH; n++) out[n] = 0;
-    memcpy(in, paddr, 6);
-    metis_digest_mac_from_str_salt((char*) in, (char*)salt, out);
+    char in[METIS_OUTPUT_HASH_LENGTH];
+    // ESP_LOGI(TAG, "salted MAC %d", in);
 
-    //metis_digest_mac_salt(in, salt, out);
+    // memcpy(in, paddr, 6);
+    metis_enable_printing(true);
+
+    /**
+     * Determinar si una dirección MAC corresponde de manera probable a un
+     * dispositivo móvil.
+     * Esta función realiza una consulta a un listado interno de la biblioteca
+     * para determinar si, por medio del fabricante indicado en la dirección
+     * MAC, el dispositivo por el que se ha consultado es susceptible de ser un
+     * Smartphone, Tablet, u otro dispositivo personal con funcionalidad WiFi
+     */
+
+    bool *isDev = false;
+    // metis_is_device((char *)in, isDev);
+    //buff  = "";
+    // char *exampleMac = str_;
+    strcpy(in, buff);
+    // metis_digest_mac_salt(in, salt, out); // Last version
+
+    // const char aaa = in;
+    // memcpy(&aaa, in, METIS_OUTPUT_HASH_LENGTH);
+
+    if (metis_digest_mac_from_str_salt(in, "FFFFFFFF", out) ==
+        metis_failure_reason_none) {
+      ESP_LOGD(TAG, "OK!\n");
+      ESP_LOGD(TAG, "Digest Mac: %s\n", out);
+    } else {
+      ESP_LOGD(TAG, "FAILED\n");
+    }
+
     memcpy(&hashedmac, out, METIS_OUTPUT_HASH_LENGTH);
-
     uint32_t hashedmacH = (hashedmac >> 32) & 0xFFFFFFFF;
-    uint32_t hashedmacL = (hashedmac) & 0xFFFFFFFF;
-    char hashedmacbuff[20];
-    snprintf(hashedmacbuff, sizeof hashedmacbuff, "%08X%08X", hashedmacH,hashedmacL);
+    uint32_t hashedmacL = (hashedmac)&0xFFFFFFFF;
 
-    switch(sniff_type)
-    {
-      case MAC_SNIFF_WIFI:
-      {
-        auto newmac = macs_list_wifi.insert(hashedmacL); // add hashed MAC, if new unique
-        added = newmac.second ? true
-                          : false; // true if hashed MAC is unique in container
-        if (added)
-        {
-          macs_wifi++; // increment Wifi MACs counter
+    //  for(int n = 0; n < METIS_OUTPUT_HASH_LENGTH; n++) in[n] = 0;
+    // for(int n = 0; n < METIS_OUTPUT_HASH_LENGTH; n++) out[n] = 0;
+
+    // ESP_LOGD(TAG, "out: %d", out);
+    char hashedmacbuff[20];
+    snprintf(hashedmacbuff, sizeof(hashedmacbuff), "%08X%08X", hashedmacH,
+             hashedmacL);
+
+    switch (sniff_type) {
+    case MAC_SNIFF_WIFI: {
+      auto newmac =
+          macs_list_wifi.insert(hashedmacL); // add hashed MAC, if new unique
+      added = newmac.second
+                  ? true
+                  : false; // true if hashed MAC is unique in container
+      if (added) {
+        macs_wifi++; // increment Wifi MACs counter
 #if (HAS_LED != NOT_A_PIN) || defined(HAS_RGB_LED)
-          blink_LED(COLOR_GREEN, 50);
+        blink_LED(COLOR_GREEN, 50);
 #endif
-        }
-        break;
       }
-      case MAC_SNIFF_BLE:
-      {
-        auto newmac = macs_list_ble.insert(hashedmacL); // add hashed MAC, if new unique
-        added = newmac.second ? true
-                          : false; // true if hashed MAC is unique in container
-        if (added)
-        {
-          macs_ble++; // increment Wifi MACs counter
+      break;
+    }
+    case MAC_SNIFF_BLE: {
+      auto newmac =
+          macs_list_ble.insert(hashedmacL); // add hashed MAC, if new unique
+      added = newmac.second
+                  ? true
+                  : false; // true if hashed MAC is unique in container
+      if (added) {
+        macs_ble++; // increment Wifi MACs counter
 #if (HAS_LED != NOT_A_PIN) || defined(HAS_RGB_LED)
-          blink_LED(COLOR_MAGENTA, 50);
+        blink_LED(COLOR_MAGENTA, 50);
 #endif
-        }
-        break;
       }
-      case MAC_SNIFF_BT:
-      {
-        auto newmac = macs_list_bt.insert(hashedmacL); // add hashed MAC, if new unique
-        added = newmac.second ? true
-                          : false; // true if hashed MAC is unique in container
-        if (added)
-        {
-          macs_bt++; // increment Wifi MACs counter
+      break;
+    }
+    case MAC_SNIFF_BT: {
+      auto newmac =
+          macs_list_bt.insert(hashedmacL); // add hashed MAC, if new unique
+      added = newmac.second
+                  ? true
+                  : false; // true if hashed MAC is unique in container
+      if (added) {
+        macs_bt++; // increment Wifi MACs counter
 #if (HAS_LED != NOT_A_PIN) || defined(HAS_RGB_LED)
-          blink_LED(COLOR_BLUE_MAGENTA, 50);
+        blink_LED(COLOR_BLUE_MAGENTA, 50);
 #endif
-        }
-        break;
+      }
+      break;
+    }
+    }
+    // in beacon monitor mode check if seen MAC is a known beacon
+    if (cfg.monitormode) {
+      beaconID = isBeacon(macConvert(paddr));
+      if (beaconID >= 0) {
+        ESP_LOGI(TAG, "Beacon ID#%d detected", beaconID);
+#if (HAS_LED != NOT_A_PIN) || defined(HAS_RGB_LED)
+        blink_LED(COLOR_WHITE, 2000);
+#endif
+        payload.reset();
+        payload.addAlarm(rssi, beaconID);
+        SendPayload(BEACONPORT, prio_high);
       }
     }
-      // in beacon monitor mode check if seen MAC is a known beacon
-      if (cfg.monitormode) {
-        beaconID = isBeacon(macConvert(paddr));
-        if (beaconID >= 0) {
-          ESP_LOGI(TAG, "Beacon ID#%d detected", beaconID);
-#if (HAS_LED != NOT_A_PIN) || defined(HAS_RGB_LED)
-          blink_LED(COLOR_WHITE, 2000);
-#endif
-          payload.reset();
-          payload.addAlarm(rssi, beaconID);
-          SendPayload(BEACONPORT, prio_high);
-        }
-      }
 
-     // added
+    // added
 
     // Log scan result
-    if(added)
-    ESP_LOGD(TAG,
-             "%s %s RSSI %ddBi -> salted MAC %s -> Hash %s -> WiFi:%d  "
-             "BLE:%d -> BLTH:%d -> "
-             "%d Bytes left",
-             added ? "new  " : "known",
-             sniff_type == MAC_SNIFF_WIFI ? "WiFi" : sniff_type == MAC_SNIFF_BLE ? "BLE" : "BLTH", rssi, buff,
-             hashedmacbuff, macs_wifi, macs_ble, macs_bt, getFreeRAM());
+    if (added) {
+      ESP_LOGD(TAG, "%d salt", salt);
+
+      ESP_LOGD(TAG,
+               "%s %s RSSI %ddBi -> salted MAC %s -> Hash %s -> WiFi:%d  "
+               "BLE:%d -> BLTH:%d -> "
+               "%d Bytes left",
+               added ? "new  " : "known",
+               sniff_type == MAC_SNIFF_WIFI  ? "WiFi"
+               : sniff_type == MAC_SNIFF_BLE ? "BLE"
+                                             : "BLTH",
+               rssi, in, hashedmacbuff, macs_wifi, macs_ble, macs_bt,
+               getFreeRAM());
+    }
 
 #if (VENDORFILTER)
   } else {

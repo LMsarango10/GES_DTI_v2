5d765af4a333bb3a6e83bf16a64fce88313ec2e2
Luigi Frassato <luigifrassato@outlook.com>
2022-08-24 01:14:46 -0300

    fix log

--- src/updates.cpp
+++ src/updates.cpp
@@ -86,7 +86,7 @@ bool getChecksumFromFile(int fileNumber, uint32_t* checksum)
   }
   checksumFile.read((uint8_t*)checksum, sizeof(checksum));
   checksumFile.close();
-  ESP_LOGV(TAG, "Value read from checksum file: %08X", checksum);
+  ESP_LOGV(TAG, "Value read from checksum file: %08X", *checksum);
   return true;
 }
 

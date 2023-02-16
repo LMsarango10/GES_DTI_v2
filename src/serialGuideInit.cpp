0ce771d5c11f4506898f41a869524c52c0f4412b
danielei9 <dbs99@hotmail.com>
2023-02-16 17:50:35 +0100

    Update serialGuideInit.cpp

--- src/serialGuideInit.cpp
+++ src/serialGuideInit.cpp
@@ -1,24 +1,35 @@
 #include "serialGuideInit.h"
 #include <EEPROM.h>
-
-static const char TAG[] = __FILE__;
+#include <lora_credentials.hpp>
 
 uint8_t __DEVEUI[8] = {0x00, 0x00, 0x00, 0x00,
-                    0x00, 0x00, 0x00, 0x00}; // 3079e129d522e14f
+                       0x00, 0x00, 0x00, 0x00}; // 3079e129d522e14f
 
-void convert(const char *s) {
-  for (int j = 0, i = 0; j < strlen(s); j++) {
+void convert(const char *s)
+{
+  for (int j = 0, i = 0; j < strlen(s); j++)
+  {
     i = round((j - 0.01) / 2);
-    if (j % 2 == 0 or j == 0) {
-      __DEVEUI[i] = __DEVEUI[i] + 16 * convertChar(s[j]);
-    } else {
-      __DEVEUI[i] = __DEVEUI[i] + convertChar(s[j]);
+    if (j % 2 == 0 or j == 0)
+    {
+      __DEVEUI[i] = (uint8_t)__DEVEUI[i] + 16 * convertChar(s[j]);
+    }
+    else
+    {
+      __DEVEUI[i] = (uint8_t)__DEVEUI[i] + convertChar(s[j]);
     }
   }
+  uint8_t temporal;
+  for (int x = 0; x < 8 / 2; x++) {
+    temporal = __DEVEUI[x];
+    __DEVEUI[x] = __DEVEUI[8 - x - 1];
+    __DEVEUI[8 - x - 1] = temporal;
+  }
 }
-
-uint8_t convertChar(char s) {
-  switch (s) {
+uint8_t convertChar(char s)
+{
+  switch (s)
+  {
   case '0':
     return 0;
   case '1':
@@ -51,78 +62,32 @@ uint8_t convertChar(char s) {
     return 14;
   case 'f':
     return 15;
+  case 'A':
+    return 10;
+  case 'B':
+    return 11;
+  case 'C':
+    return 12;
+  case 'D':
+    return 13;
+  case 'E':
+    return 14;
+  case 'F':
+    return 15;
 
   default:
     Serial.print("NOT VALID HEX");
     break;
   }
 }
-
-void initEeprom() { EEPROM.begin(1024); }
-
-void saveInitialConfig() {
-  uint32_t eepAddr = 200;
-  uint8_t configured = 7;
-
-  EEPROM.put(eepAddr, configured);
-  eepAddr += sizeof(configured);
-
-  for (int i = 0; i < 8; i++) {
-    EEPROM.put(eepAddr, DEVEUI[i]);
-    eepAddr += sizeof(DEVEUI[i]);
-  }
-
-  EEPROM.commit();
-
-  Serial.printf("Guardando la configuración en: %d\n", configured);
-  Serial.printf("Guardado\n");
-}
-
-bool readConfig() {
-  ESP_LOGI(TAG, "Leyendo la configuración existente");
-
-  uint32_t eepAddr = 200;
-  uint8_t configured = 0;
-
-  EEPROM.get(eepAddr, configured);
-  eepAddr += sizeof(configured);
-
-  if (configured != 7) {
-    printf("No existe Configuración Inicial... CONFIGURED: %d \n", configured);
-    return false;
-  }
-  if (configured == 7) {
-    printf("SI existe Configuración Inicial... CONFIGURED: %d \n", configured);
-  }
-  Serial.print("DEVEUI: ");
-
-  for (int i = 0; i < 8; i++) {
-    EEPROM.get(eepAddr, DEVEUI[i]);
-    eepAddr += sizeof(DEVEUI[i]);
-    Serial.printf("%02X", DEVEUI[i]);
-  }
-  Serial.println();
-
-  return true;
-}
-
-void initConfig() {
-  Serial.println("Bienvenido a la guía de STA\n");
-
-  Serial.println("Introduzca la deveui: ");
-  String devEui = "";
-  char character;
-  while (strlen(devEui.c_str()) < 16) {
-    if (Serial.available()) {
-      character = Serial.read();
-      Serial.print(character);
-      devEui.concat(character);
-      devEui.trim();
-    }
-  }
-  Serial.println();
-
+#define DEBUG_DEVEUI 
+void initConfigSerialGuide(String devEui,uint8_t actualConfig)
+{
   convert(devEui.c_str());
+#ifdef DEBUG_DEVEUI
+  Serial.println(devEui);
+  Serial.println("SIZEOF");
+  Serial.println(sizeof(devEui));
   Serial.print(__DEVEUI[0], HEX);
   Serial.print(__DEVEUI[1], HEX);
   Serial.print(__DEVEUI[2], HEX);
@@ -133,51 +98,36 @@ void initConfig() {
   Serial.println(__DEVEUI[7], HEX);
 
   Serial.println("Nueva DevEUI Añadida: ");
-  for (int i = 0; i < 8; i++) {
-    DEVEUI[i] = __DEVEUI[i];
-    Serial.print(DEVEUI[i], HEX);
+
+  for (int i = 0; i < 8; i++)
+  {
+    DEVEUI_DEF[i] = __DEVEUI[i];
+    Serial.print(DEVEUI_DEF[i], HEX);
   }
 
   Serial.print("\n");
   Serial.print("\n");
-  Serial.print("DEVEUI OK");
-  Serial.print("\n");
-  Serial.print("\n");
-  saveInitialConfig();
+#endif
+  // memcpy(DEVEUI_DEF, __DEVEUI, 8);
+  uint32_t eepAddr = 0;
+  // config
+  EEPROM.put(eepAddr, actualConfig);
+  eepAddr += sizeof(actualConfig);
+
+  // Deveui
+  Serial.printf("Setting DEVEUI to eeprom: ");
+  for (int i = 0; i < 8; i++)
+  {
+    EEPROM.put(eepAddr, DEVEUI_DEF[i]);
+    eepAddr += sizeof(DEVEUI_DEF[i]);
+    Serial.print(DEVEUI_DEF[i], HEX);
+  }
+  Serial.printf("\n      * Ok\n");
+  EEPROM.commit();
+  printf("Saved config eeprom\n");
+  // saveInitialConfig();
 }
-void checkConfig() {
-  Serial.print("APPKEY: ");
 
-  for (int i = 0; i < 16; i++) {
-    Serial.print(APPKEY[i], HEX);
-  }
-  Serial.println();
-  initEeprom();
-  // Si no hay configuración
-  if (!readConfig()) {
-    // Iniciar lectura de configuración
-    initConfig();
-  }
-  // Si, si que hay configuración inicial
-  else {
-    Serial.print("Quieres resetear la configuración? Teclee Y = SI / N = NO "
-                 "...( Tienes 10 segundos para contestar )\n");
-    double lastMillis = 0;
-    lastMillis = millis();
-    while (millis() - lastMillis < 10000) {
-      if (Serial.available()) {
-        char character = Serial.read();
-        if (character == 'Y' || character =='y') {
-          EEPROM.put(200, 5);
-          EEPROM.commit();
-          Serial.print("Reiniciando...");
-          ESP.restart();
-        }
-        if (character == 'N' || character == 'n') {
-          break;
-        }
-      }
-    }
-  }
-  delay(2000);
+void saveDeveuiEeprom(){
+
 }
